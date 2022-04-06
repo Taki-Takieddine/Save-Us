ba00c170afe77cded85a2aacd2a20581fe64f51d
radiasahraoui <radiasahraoui7@gmail.com>
2022-04-07 00:34:19 +0200

    log in screen update

--- lib/auth/widget/input_field.dart
+++ lib/auth/widget/input_field.dart
@@ -5,39 +5,54 @@ class InputFeild extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          padding: const EdgeInsets.all(10),
-          decoration: const BoxDecoration(
-              border: Border(
-                  bottom: BorderSide(
-            color: Colors.grey,
-          ))),
-          child: const TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-                hintText: "name@gmail.com",
-                hintStyle: TextStyle(color: Colors.grey),
-                border: InputBorder.none),
-          ),
+    final GlobalKey<FormState> _formKey = GlobalKey();
+    return Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'E-mail',
+                border: OutlineInputBorder(),
+                prefixIcon:   Icon(Icons.email,color: Colors.purple,),
+                labelStyle: TextStyle(
+                  fontSize: 15.0,
+                ),
+              ),
+              keyboardType: TextInputType.emailAddress,
+              validator: (value) {
+                if (value!.isEmpty  && value.contains('@')) {
+                  return 'Email invalid';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                
+              },
+            ),
+          const  SizedBox(height: 30,),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Password',border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.transparent,
+                ),
+               
+              ),
+              
+              prefixIcon:   Icon(Icons.password_rounded, color: Colors.purple),),
+              
+              obscureText: true,
+              validator: (value) {
+                if (value!.isEmpty && value.length < 6) {
+                  return 'mot de passe trop court!';
+                }
+                return null;
+              },
+              onSaved: (value) {
+               
+              },
+            ),
+          ],
         ),
-        Container(
-          padding: const EdgeInsets.all(10),
-          decoration: const BoxDecoration(
-              border: Border(
-                  bottom: BorderSide(
-            color: Colors.grey,
-          ))),
-          child: const TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-                hintText: "mot de passe",
-                hintStyle: TextStyle(color: Colors.grey),
-                border: InputBorder.none),
-          ),
-        )
-      ],
-    );
-  }
-}
+      );
+      }}
