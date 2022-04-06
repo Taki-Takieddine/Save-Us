de5612ab13252ac7d98c6a0a5b6314e8c015c2d3
radiasahraoui <radiasahraoui7@gmail.com>
2022-04-07 01:34:54 +0200

    ui signaleur

--- lib/auth/widget/button.dart
+++ lib/auth/widget/button.dart
@@ -1,11 +1,21 @@
- import 'package:flutter/material.dart';
+import 'package:applicationmemoire/screen/signaleur.dart';
+import 'package:flutter/material.dart';
+
 
 class Button extends StatelessWidget {
   const Button({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+
+        return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+           MaterialPageRoute(builder: ((context) => const Signaleur())));
+      },
+     child: 
+    Container(
       height: 50,
       margin: const EdgeInsets.symmetric(horizontal: 50),
       decoration: BoxDecoration(
@@ -13,13 +23,14 @@ class Button extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
       ),
       child: const Center(
-        
+
         child: Text("Log In",style: TextStyle(
           color: Colors.white,
           fontSize: 15,
           fontWeight: FontWeight.bold,
           ),),
       ),
+    )
     );
   }
 }
---
+++ lib/screen/signaleur.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+class Signaleur extends StatelessWidget {
+  const Signaleur({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Signaleur") ,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+
+        onPressed: () {},
+          child: Icon(Icons.pin_drop_outlined),
+
+
+      ),
+    );
+  } 
+}
