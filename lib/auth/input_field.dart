de5f29daefe745846658a13b6bafae1b7dd14f37
toubal77 <72252709+toubal77@users.noreply.github.com>
2022-04-06 22:57:49 +0200

    make type input textField

--- lib/auth/input_field.dart
+++ lib/auth/input_field.dart
@@ -1,48 +1,43 @@
 import 'package:flutter/material.dart';
 
- class InputFeild extends StatelessWidget{
+class InputFeild extends StatelessWidget {
   const InputFeild({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-   return Column(
-     children: <Widget>[
+    return Column(
+      children: <Widget>[
         Container(
-    padding: const EdgeInsets.all(10),
-    decoration: const BoxDecoration(
-      border: Border(
-        bottom: BorderSide(
-          color: Colors.grey,
+          padding: const EdgeInsets.all(10),
+          decoration: const BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(
+            color: Colors.grey,
+          ))),
+          child: const TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+                hintText: "name@gmail.com",
+                hintStyle: TextStyle(color: Colors.grey),
+                border: InputBorder.none),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(10),
+          decoration: const BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(
+            color: Colors.grey,
+          ))),
+          child: const TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                hintText: "mot de passe",
+                hintStyle: TextStyle(color: Colors.grey),
+                border: InputBorder.none),
+          ),
         )
-      )
-    ),
-    child: const TextField(
-      decoration: InputDecoration(
-        hintText: "name@gmail.com",
-        hintStyle: TextStyle(color: Colors.grey),
-        border: InputBorder.none
-      ),
-    ),
-   ),
-    Container(
-    padding: const EdgeInsets.all(10),
-    decoration: const BoxDecoration(
-      border: Border(
-        bottom: BorderSide(
-          color: Colors.grey,
-        )
-      )
-    ),
-    child: const TextField(
-      decoration: InputDecoration(
-        hintText: "mot de passe",
-        hintStyle: TextStyle(color: Colors.grey),
-        border: InputBorder.none
-      ),
-    ),
-   )
-     ],
-   );
+      ],
+    );
   }
-  
 }
