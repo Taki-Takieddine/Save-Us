16d78d72c78339a57ef3f18f6df2a80c3cf9e562
radiasahraoui <radiasahraoui7@gmail.com>
2022-04-24 01:19:50 +0200

    overflowing problem solved (resto)

--- lib/screen/restaurent.dart
+++ lib/screen/restaurent.dart
@@ -23,147 +23,151 @@ class _Restaurent extends State<Restaurent> {
  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Restaurant") ,
-        backgroundColor: const Color.fromARGB(255, 53, 119, 174),
-      ),
-
-
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: <Color>[
-              Color.fromARGB(255, 53, 119, 174),
-              Color.fromARGB(255, 109, 189, 112)
-            ]
-            )
-            ),
-
-            child: Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-              const SizedBox(
-                height: 25,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                
-                children: [
-                  Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.all(15),
-                      height: 150,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(        
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.transparent.withOpacity(0.2),
-                      ),
-                      child:  Center(
-                        child: Column(
-                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                           Text("$nombresac Sacs",style: const TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
-                           
-                     Row(
-                       
-                       mainAxisAlignment: MainAxisAlignment.center,
-                       children: [
-                         Container(
-                            decoration:  BoxDecoration(
-                         color: Colors.white,
-                         borderRadius: BorderRadius.circular(50),),
-                           child: Center(
-                             child:IconButton(
-                           onPressed:() {
-                             setState(() {
-                               if(nombresac>0){
-                               nombresac-=1;
-                               }
-                             });
-                           }, icon: const Icon(Icons.remove_outlined,color: Color.fromARGB(255, 53, 119, 174),),
-                           
-                         ),),
-                         ),
-                         
-                           const SizedBox(
-                          width: 20,
-                  ),
-                     Container(
-                        decoration:  BoxDecoration(
-                         color: Colors.white,
-                         borderRadius: BorderRadius.circular(50)
-                       ),
-                       child: Center(
-                         child: IconButton(
-                           onPressed:() {
-                             setState(() {
-                               nombresac+=1;
-                             });
-                           },
-                           icon: const Icon(Icons.add,color: Color.fromARGB(255, 53, 119, 174),),
-                           ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Restaurant") ,
+          backgroundColor: const Color.fromARGB(255, 53, 119, 174),
+        ),
+    
+    
+        body: SafeArea(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: <Color>[
+                  Color.fromARGB(255, 53, 119, 174),
+                  Color.fromARGB(255, 109, 189, 112)
+                ]
+                )
+                ),
+        
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                const SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: const EdgeInsets.all(15),
+                        height: 150,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(        
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.transparent.withOpacity(0.2),
                         ),
-                     )    
-                       ],
-                     ),
-                         ], ),
+                        child:  Center(
+                          child: Column(
+                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                             Text("$nombresac Sacs",style: const TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
+                             
+                       Row(
+                         
+                         mainAxisAlignment: MainAxisAlignment.center,
+                         children: [
+                           Container(
+                              decoration:  BoxDecoration(
+                           color: Colors.white,
+                           borderRadius: BorderRadius.circular(50),),
+                             child: Center(
+                               child:IconButton(
+                             onPressed:() {
+                               setState(() {
+                                 if(nombresac>0){
+                                 nombresac-=1;
+                                 }
+                               });
+                             }, icon: const Icon(Icons.remove_outlined,color: Color.fromARGB(255, 53, 119, 174),),
+                             
+                           ),),
+                           ),
+                           
+                             const SizedBox(
+                            width: 20,
                     ),
-                  ),
-                  ),
-                   Expanded(
-                    child: Container(
-                       margin: const EdgeInsets.all(15),
-                      height: 150,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(        
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.transparent.withOpacity(0.2),
+                       Container(
+                        
+                          decoration:  BoxDecoration(
+                           color: Colors.white,
+                           borderRadius: BorderRadius.circular(50)
+                         ),
+                         child: Center(
+                           child: IconButton(
+                             onPressed:() {
+                               setState(() {
+                                 nombresac+=1;
+                               });
+                             },
+                             icon: const Icon(Icons.add,color: Color.fromARGB(255, 53, 119, 174),),
+                             ),
+                          ),
+                       )    
+                         ],
+                       ),
+                           ], ),
                       ),
-                      child: const Icon(Icons.star_rounded ,color: Colors.white,size: 50,),
                     ),
-                  ),
-                ],
-              ),
-              Expanded(
+                    ),
+                     Expanded(
+                      child: Container(
+                    
+                         margin: const EdgeInsets.all(15),
+                        height: 150,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(        
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.transparent.withOpacity(0.2),
+                        ),
+                        child: const Icon(Icons.star_rounded ,color: Colors.white,size: 50,),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
                   child: Container(
                      width: MediaQuery.of(context).size.width,
-                decoration: const BoxDecoration(
+                     height: MediaQuery.of(context).size.height,
+                    decoration: const BoxDecoration(
                     color: Colors.white,
                     borderRadius:
                         BorderRadius.only(topRight: Radius.circular(60),  
                      ),
                   ),
-                  child: Expanded(
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 10,),
-                        const Text("Status des sacs",style: TextStyle(color: Color.fromARGB(255, 53, 119, 174,),fontWeight: FontWeight.bold,fontSize: 30),),  
-                        Container(
-                                        
-                                        width: MediaQuery.of(context).size.width,
-                                        margin: const EdgeInsets.all(25),
-                                        decoration: BoxDecoration(        
-                        
-                        border: Border.all(color: Colors.grey),
-                                        ),
-                                                child:Container(
-                                                  width:MediaQuery.of(context).size.width ,
-                                                  margin: const EdgeInsets.all(20),
-                                                  height: 350,
-                                                  child: const Text('ko n3amrha ki ndir BDD')),
-                        )],
-                      ),
-                  ),
-                 ),
-              ),
-                      ],
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 10,),
+                      const Text("Status des sacs",style: TextStyle(color: Color.fromARGB(255, 53, 119, 174,),fontWeight: FontWeight.bold,fontSize: 30),),  
+                      Container(
+                                      
+                                      width: MediaQuery.of(context).size.width,
+                                      margin: const EdgeInsets.all(25),
+                                      decoration: BoxDecoration(        
+                      
+                      border: Border.all(color: Colors.grey),
+                                      ),
+                                              child:Container(
+                                                width:MediaQuery.of(context).size.width ,
+                                                margin: const EdgeInsets.all(20),
+                                                height: 350,
+                                                child: const Text('ko n3amrha ki ndir BDD')),
+                      )],
+                    ),
                      ),
-            )
+                ),
+                        ],
+                       )
+          ),
+        ),
       ),
     );
   } 
